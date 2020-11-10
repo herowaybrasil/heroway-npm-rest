a831c5d1714aaae382d42bd6db9b194304afd0a5
Gtosta96 <gabrieltosta3@gmail.com>
2020-11-10 16:24:54 -0300

    minor

--- src/modules/cowsay.js
+++ src/modules/cowsay.js
@@ -1,12 +1,13 @@
-// const cowsay = require('cowsay');
+// habilitar "type": "commonjs" no package.json
+const cowsay = require('cowsay');
 
 // habilitar "type": "module" no package.json
-import cowsay from 'cowsay';
+// import cowsay from 'cowsay';
 
-import * as myModule from './my-module.js';
+// import * as myModule from './my-module.js';
 // import greeting, { company } from './my-module.js';
 
-console.log(myModule);
+// console.log(myModule);
 
 const result = cowsay.say({text: 'Im a moooodule', e : 'oO', T : 'U ' });
 console.log(result);
